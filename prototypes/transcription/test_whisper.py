1419a01a6be76f2c71491ac87a7ea0bb8bd0ce8b
Sean Ow <sean.ow25@gmail.com>
2025-12-11 16:26:23 +0800

    test_whisper.py: Set up API Caller

---
+++ prototypes/transcription/test_whisper.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Load .env variables
+load_dotenv()
+
+client = OpenAI(
+  api_key=os.getenv("OPENAI_API_KEY")
+)
+
+response = client.responses.create(
+  model="gpt-5-nano",
+  input="write a haiku about ai",
+  store=True,
+)
+
+print(response.output_text)
