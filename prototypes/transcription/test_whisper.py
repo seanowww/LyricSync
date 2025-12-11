c176be024376336a64771d8913962a69f89482e1
Sean Ow <sean.ow25@gmail.com>
2025-12-11 16:40:50 +0800

    Integrate Whisper API

--- prototypes/transcription/test_whisper.py
+++ prototypes/transcription/test_whisper.py
@@ -1,18 +1,26 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from pathlib import Path
 
 # Load .env variables
 load_dotenv()
+
+# --- Path handling ---
+BASE_DIR = Path(__file__).resolve().parent
+audio_path = BASE_DIR / "audio" / "test_cover_1.mp3"
+
+print("Loading file:", audio_path)
 
 client = OpenAI(
   api_key=os.getenv("OPENAI_API_KEY")
 )
 
-response = client.responses.create(
-  model="gpt-5-nano",
-  input="write a haiku about ai",
-  store=True,
-)
+with open(audio_path, "rb") as audio_file:
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        response_format="verbose_json"
+    )
 
-print(response.output_text)
+print(transcript)
