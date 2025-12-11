18a148f82cd64997302495430d465dec4f258daf
Sean Ow <sean.ow25@gmail.com>
2025-12-11 20:54:32 +0800

    Implement end-to-end flow

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
 # src/main.py
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 import shutil
 import tempfile
+
+load_dotenv()
 
 from .timing_pipeline import generate_timing_segments
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],    # allow POST, GET, etc.
+    allow_headers=["*"],    # allow Content-Type, Authorization, etc.
+)
 
 #User posts video file
 @app.post("/api/transcribe")
